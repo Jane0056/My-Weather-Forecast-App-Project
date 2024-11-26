adf355ae3df099634d3efe204392a2009f47ec90
Jane0056 <159496358+Jane0056@users.noreply.github.com>
2024-11-26 20:33:10 +0100

    Added Function to format the date and time

--- script.js
+++ script.js
@@ -29,3 +29,25 @@ function weatherDetail(response) {
   // Call forecast function
   getForecast(cityName);
 }
+
+// Function to format the date and time
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
